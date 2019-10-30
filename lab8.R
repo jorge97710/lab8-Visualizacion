729aba27557d6160c4e614294c860ca72ea76aeb
Michelle Bloomfield <blo16803@uvg.edu.gt>
2019-10-30 13:28:36 -0600

    changes

--- lab8.R
+++ lab8.R
@@ -1,7 +1,14 @@
-
+library(ggplot2)
 
 setwd("C:/Users/User/Desktop/lab8-Visualizacion")
-l <- read.table("2016.txt", sep ="|")
+
 
 #segunda manera de cargar la data, yo cambie todas las | por , pero se puede hacer por cualquier manera cambiando sep.
-data <- read.table("web_imp_08012017.txt", sep=",", header=TRUE, , row.names=NULL)
+data_2016 <- read.table("2016.txt", sep="|", header=TRUE, row.names=NULL)
+data_2017 <- read.table("2017.txt", sep="|", header=TRUE, row.names=NULL)
+data_2018 <- read.table("2018.txt", sep="|", header=TRUE, row.names=NULL)
+data_2019 <- read.table("2019.txt", sep="|", header=TRUE, row.names=NULL)
+
+datos <- rbind(data_2017, data_2018, data_2019, data_2016)
+
+summary(datos)
