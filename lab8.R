1540fb8756a315cf32bb4b5bfbe5b9c2a6cc4984
Ana lucia Diaz <LEPPEDIAZ@users.noreply.github.com>
2019-10-30 12:21:26 -0600

    cargando data en R

--- lab8.R
+++ lab8.R
@@ -2,3 +2,6 @@
 
 setwd("C:/Users/User/Desktop/lab8-Visualizacion")
 l <- read.table("2016.txt", sep ="|")
+
+#segunda manera de cargar la data, yo cambie todas las | por , pero se puede hacer por cualquier manera cambiando sep.
+data <- read.table("web_imp_08012017.txt", sep=",", header=TRUE, , row.names=NULL)
